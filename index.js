75a8283634fe688358b0da8f60971bdb0147f580
nafisa-am <nafisa.am91@gmail.com>
2022-12-04 20:35:47 +0000

    date formatter

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/wy-network', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sna-network', 
+{
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
