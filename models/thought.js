7de016d87ad2905ac7902d5129817d3d4ae1d084
nafisa-am <nafisa.am91@gmail.com>
2022-12-04 17:34:26 +0000

    reaction schema

--- models/thought.js
+++ models/thought.js
@@ -1,6 +1,37 @@
 const { Schema, model, Types } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
+// Reaction Achema 
+const ReactionSchema = new Schema(
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal),
+      },
+    },
+    {
+      toJSON: {
+        getter: true,
+      },
+    }
+  );
+
+//   Thought Schema
+
 const ThoughtSchema = new Schema(
   {
     thoughtText: {
@@ -27,6 +58,11 @@ const ThoughtSchema = new Schema(
     },
   }
 );
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+  });
+  
 
 const Thought = model("Thought", ThoughtSchema);
 
