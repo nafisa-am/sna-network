9c3821b86a76868106687a0b47d1a2938d662400
nafisa-am <nafisa.am91@gmail.com>
2022-12-04 18:05:21 +0000

    getting and creating a thought in controllers

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -1,1 +1,27 @@
+const { Thought, User } = require("../models");
 
+const thoughtController = {
+    // Getting all thoughts 
+    
+
+    async getAllThoughts(req, res) {
+        try {
+          const dbThought = await Thought.find({}).populate({ path: "reactions", select: "-__v" }).select("-__v");
+          if (!dbThought) return res.status(400).json({ message: "Bad Request!" });
+          res.json(dbThought);
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      },
+       // Creating a thought
+    async createThought({ body }, res) {
+        try {
+          const dbThought = await Thought.create(body);
+          const dbUser = await User.findOneAndUpdate({ _id: body.userId }, { $push: { thoughts: dbThought.id } }, { new: true });
+          res.json({ dbThought, dbUser, message: "A thought was successfully created" });
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      }
+    };
+    
